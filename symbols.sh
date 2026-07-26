897aa65be3f13cefca39370e612a6768e07c652a
appleflyerv3 <164886846+appleflyerv3@users.noreply.github.com>
2026-07-26 18:49:29 +0800

    fix: forgot to add symbol
    Signed-off-by: appleflyerv3 <164886846+appleflyerv3@users.noreply.github.com>

--- symbols.sh
+++ symbols.sh
@@ -589,6 +589,7 @@ tcg_gen_exit_tb \
 tcg_gen_goto_tb \
 tcg_gen_lookup_and_goto_ptr \
 check_exit_request \
+check_exit_request_safe \
 tcg_gen_qemu_ld_i32 \
 tcg_gen_qemu_st_i32 \
 tcg_gen_qemu_ld_i64 \
